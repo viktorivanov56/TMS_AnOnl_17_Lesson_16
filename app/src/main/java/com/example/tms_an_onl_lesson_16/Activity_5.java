2e2469b4ebb661e2df49718ae64647d59e220082
viktorivanov56 <Viktor.ivanov.524101630@gmail.com>
2024-02-17 15:31:25 +0300

    Lesson

---
+++ app/src/main/java/com/example/tms_an_onl_lesson_16/Activity_5.java
@@ -0,0 +1,31 @@
+package com.example.tms_an_onl_lesson_16;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Activity_5 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_5);
+        Button btnGoToMainAct = (Button) findViewById(R.id.btnToMainAct);
+
+        View.OnClickListener oclBtnGoToMainAct = new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Intent intent = new Intent(Activity_5.this, MainActivity.class);
+                startActivity(intent);
+
+            }
+        };
+
+        btnGoToMainAct.setOnClickListener(oclBtnGoToMainAct);
+    }
+}
